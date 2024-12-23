576516107dfa988c0ab0d022599d2d72c8dfc8b9
A <ui.builder22@gmail.com>
2024-12-24 01:16:03 +0530

    Revert "cors fix"
    This reverts commit d00cdf6cad21757d527909ff9661fa20fcef2c50.

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,16 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: '*', // Allow access from any domain
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeadersOnError: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
