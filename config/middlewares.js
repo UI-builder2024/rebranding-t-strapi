b4e4b4dcb611261bc2cb7aa02cb1c1a5f5d209a4
A <ui.builder22@gmail.com>
2024-12-31 20:22:59 +0530

    Fixed file size.

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,5 @@
+// config/middlewares.js
+
 module.exports = [
   'strapi::logger',
   'strapi::errors',
@@ -5,7 +7,18 @@ module.exports = [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  // Remove 'strapi::body' string and replace with an object:
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '100mb',    // Set form data size limit
+      jsonLimit: '100mb',    // JSON body size limit
+      textLimit: '100mb',    // Text body size limit
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024, // 100MB in bytes
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
